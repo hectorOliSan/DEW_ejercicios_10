d888793dba030cc90106fe70fb46abceafedc45c
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2023-02-13 08:35:45 +0000

    Update filtros.js

--- js/filtros.js
+++ js/filtros.js
@@ -7,18 +7,22 @@ const situar = document.getElementById("situar");
 imagen.style.left = "50px";
 imagen.style.transition = "all 0.3s ease-in-out";
 
+// Izquierda
 btn_i.addEventListener("click", () => {
+  imagen.style.transform = "scaleX(-1)";
   const pos = Number(imagen.style.left.split("px")[0]) - 200;
-  if (imagen.offsetLeft - imagen.offsetWidth - 200 <= 10) {
-    imagen.style.left = "0px";
-  } else imagen.style.left = pos + "px";
+  if (imagen.offsetLeft - imagen.offsetWidth - 200 >= 10) {
+    imagen.style.left = pos + "px";
+  } else imagen.style.left = "0px";
 });
 
+// Derecha
 btn_d.addEventListener("click", () => {
+  imagen.style.transform = "scaleX(1)";
   const pos = Number(imagen.style.left.split("px")[0]) + 200;
-  if (imagen.offsetLeft + imagen.offsetWidth + 200 >= window.innerWidth) {
-    imagen.style.left = window.innerWidth - imagen.offsetWidth + "px";
-  } else imagen.style.left = pos + "px";
+  if (imagen.offsetLeft + imagen.offsetWidth + 200 <= window.innerWidth) {
+    imagen.style.left = pos + "px";
+  } else imagen.style.left = window.innerWidth - imagen.offsetWidth + "px";
 });
 
 window.addEventListener("resize", () => {
@@ -27,4 +31,7 @@ window.addEventListener("resize", () => {
   }
 });
 
-situar.addEventListener("click", () => (imagen.style.left = "50px"));
+situar.addEventListener("click", () => {
+  imagen.style.left = "50px";
+  imagen.style.transform = "scaleX(1)";
+});
